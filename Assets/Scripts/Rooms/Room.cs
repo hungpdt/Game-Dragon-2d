275f7e7d566909f5a8ca6caac901afd6483ab6ab
hungpdt <pidithiet@gmail.com>
2024-07-02 08:11:34 +0700

    *fixed bug fireball don't active when switch room

--- Assets/Scripts/Rooms/Room.cs
+++ Assets/Scripts/Rooms/Room.cs
@@ -5,31 +5,44 @@ public class Room : MonoBehaviour
 {
     [SerializeField] private GameObject[] enemies;
     private Vector3[] initialPosition;
+    private bool[] initialActiveStates;
 
     private void Awake()
     {
-        //Save the initial positions of the enemies
+        // Save the initial positions and active states of the enemies
         initialPosition = new Vector3[enemies.Length];
-        for (int i = 0; i < enemies.Length; i++)
-        {
-            if(enemies[i] != null)
-                initialPosition[i] = enemies[i].transform.position;
-        }
-
-        //Deactivate rooms
-        if (transform.GetSiblingIndex() != 0)
-            ActivateRoom(false);
-    }
-    public void ActivateRoom(bool _status)
-    {
-        //Activate/deactivate enemies
+        initialActiveStates = new bool[enemies.Length];
         for (int i = 0; i < enemies.Length; i++)
         {
             if (enemies[i] != null)
             {
-                enemies[i].SetActive(_status);
-                enemies[i].transform.position = initialPosition[i];
-                Debug.Log("Setactive = "+_status + " enemy " + enemies[i].name);
+                initialPosition[i] = enemies[i].transform.position;
+                initialActiveStates[i] = enemies[i].activeSelf;
+            }
+        }
+
+        // Deactivate rooms if it's not the first room
+        if (transform.GetSiblingIndex() != 0)
+            ActivateRoom(false);
+    }
+
+    public void ActivateRoom(bool _status)
+    {
+        // Activate/deactivate enemies
+        for (int i = 0; i < enemies.Length; i++)
+        {
+            if (enemies[i] != null)
+            {
+                if (_status)
+                {
+                    enemies[i].SetActive(initialActiveStates[i]);
+                    enemies[i].transform.position = initialPosition[i];
+                }
+                else
+                {
+                    enemies[i].SetActive(false);
+                }
+                Debug.Log("SetActive = " + _status + " enemy " + enemies[i].name);
             }
         }
     }
